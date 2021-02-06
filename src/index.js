dbde783741ad8138a93ff147e4b7f67db183c580
Zehua Lin <zlin@snapchat.com>
2021-02-05 16:42:08 -0800

    mini css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('Hello');
+import('./style.css');
+
+document.body.innerText = 'hello';
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,42 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            ignoreOrder: true,
+            filename: '[fullhash].css',
+            chunkFilename: '[fullhash].chunk.css',
+        }),
+        new HtmlWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    cache: {
-        type: 'filesystem',
-        cacheDirectory: path.resolve(__dirname, '.webpack_cache'),
-    },
-    infrastructureLogging: {
-      debug: /webpack\.cache/
+        filename: '[contenthash].js',
+        chunkFilename: '[contenthash].chunk.js',
     },
 };
